e8b324a13aa27765f548d4b1d5d6f78e6b16d019
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-03-20 00:31:44 +0600

    Database connection and migration added

---
+++ ERP.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Persistence.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
